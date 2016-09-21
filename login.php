5355b04690be175326a1bcf4e479115b13961e17
varian97 <variancaesar@gmail.com>
2016-09-21 22:10:10 +0700

    update login.php

--- login.php
+++ login.php
@@ -1,35 +1,43 @@
+<?php
+	$host = "localhost:4433";
+	$db_username = "root";
+	$db_password = "";
+	$db_name = "db1";
+
+	mysql_connect($host,$db_username,$db_password);
+	mysql_select_db($db_name);
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		$sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password' LIMIT 1";
+		$result = mysql_query($sql);
+		if(mysql_num_rows($result) == 1){
+			echo "Welcome back, you have successfully logged in !";
+			exit();
+		}	
+		else {
+			echo "Invalid Login, check password or username !";
+			exit();
+		}
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Login Page</title>
 	</head>
 	<body>
-		<?php
-			include(config.php);
-			session_start();
-
-			if($_SERVER["REQUEST_METHOD"] == "POST"){
-				  // username and password sent from form 
-				  $myusername = mysqli_real_escape_string($db,$_POST['username']);
-				  $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-				  
-				  $sql = "SELECT id FROM user WHERE username = '$myusername' and passcode = '$mypassword'";
-				  $result = mysqli_query($db,$sql);
-				  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-				  $active = $row['active'];
-				  
-				  $count = mysqli_num_rows($result);
-				  
-				  // If result matched $myusername and $mypassword, table row must be 1 row					
-				  if($count == 1) {
-				     session_register("myusername");
-				     $_SESSION['login_user'] = $myusername;
-				     echo "Hello, Welcome back $myusername !";
-				     //header("location: welcome.php");
-				  }else {
-				     $error = "Your Login Name or Password is invalid";
-				  }
-			}
-		?>
+		<h1 style="text-align: center;">Sale<span style="color: blue">Project</span></h1>
+		<form method="POST" action="login.php">
+			<h2 style="border-bottom: 1px solid;margin-bottom: 10px;">Login Here</h2>
+			Email or Username :<br>
+			<input type="text" name="username"><br>
+			Password :<br>
+			<input type="password" name="password"><br>
+			<button type="button" style="float: right;">Login</button>
+			
+		</form>
 	</body>
 </html>
