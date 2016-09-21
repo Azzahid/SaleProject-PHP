97a99ca33b0b82d9a16205e942dd9af924e864c7
varian97 <variancaesar@gmail.com>
2016-09-22 00:35:02 +0700

    login.php done

--- login.php
+++ login.php
@@ -1,28 +1,3 @@
-<?php
-	$host = "localhost:4433";
-	$db_username = "root";
-	$db_password = "";
-	$db_name = "db1";
-
-	mysql_connect($host,$db_username,$db_password);
-	mysql_select_db($db_name);
-
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	if($_SERVER["REQUEST_METHOD"] == "POST"){
-		$sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password' LIMIT 1";
-		$result = mysql_query($sql);
-		if(mysql_num_rows($result) == 1){
-			echo "Welcome back, you have successfully logged in !";
-			exit();
-		}	
-		else {
-			echo "Invalid Login, check password or username !";
-			exit();
-		}
-	}
-?>
-
 <!DOCTYPE html>
 <html>
 	<head>
@@ -36,8 +11,49 @@
 			<input type="text" name="username"><br>
 			Password :<br>
 			<input type="password" name="password"><br>
-			<button type="button" style="float: right;">Login</button>
+			<input type="submit" value="Login" name="login" style="float: right;">
+			<!-- <button type="button" style="float: right;">Login</button> -->
 			
 		</form>
+		
+		<?php
+			$host = "localhost";
+			$db_username = "root";
+			$db_password = "";
+			$db_name = "db1";
+
+			$conn = mysqli_connect($host,$db_username,$db_password,$db_name);			
+			//check connection
+			if(!$conn)
+				die("Connection failed : " . mysqli_connect_error());
+			
+			if(isset($_POST['login'])){
+				$username = mysqli_real_escape_string($conn, $_POST['username']);
+				$password = mysqli_real_escape_string($conn, $_POST['password']);
+				$query = "SELECT * FROM user WHERE username = '$username' AND password = '$password' LIMIT 1";
+				$result = mysqli_query($conn,$query);
+				if(mysqli_num_rows($result) == 1){
+					//echo "Login succeeded";
+					header('Location: home.html');
+				} else {
+					echo "Wrong username or password";
+				}
+			}
+			/*
+			if($_SERVER["REQUEST_METHOD"] == "POST"){
+				$username = $_POST['username'];
+				$password = $_POST['password'];
+				$sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password' LIMIT 1";
+				//$result = mysqli_query($conn, $sql);
+				if(mysqli_query($conn, $sql)){
+					echo "Welcome back, you have successfully logged in !";
+					exit();
+				}	
+				else {
+					echo "Invalid Login, check password or username !";
+					exit();
+				}
+			}*/
+		?>
 	</body>
 </html>
