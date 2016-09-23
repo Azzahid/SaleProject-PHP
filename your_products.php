a96338f6c5b4b1e1aec9b5e0fefd42c00ea80db3
han_majid <han.majid@yahoo.co.id>
2016-09-23 19:15:57 +0700

    create your_products.php

---
+++ your_products.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Your Products</title>
+	</head>
+	<body>
+		<h1>SaleProject</h1>
+		<p>Hi, chirastore!</p>
+		<p><a href="#">logout</a></p>
+		<ul>
+			<li><a href="">Catalog</a></li>
+			<li><a href="">Your Products</a></li>
+			<li><a href="">Add Product</a></li>
+			<li><a href="">Sales</a></li>
+			<li><a href="">Purchases</a></li>
+		</ul>
+		<h3>What are you going to sell today?</h3>
+		<hr />
+		<p><strong>Sunday, 14 September 2016</strong></p>
+		<p>at 20.00</p>
+		<hr />
+		
+		<img src="http://placekitten.com/100/100">
+		<h4>Phelps Air Styler</h4>
+		<p>IDR 500.000</p>
+		<p>Brush shaped hair dryer. Very convenient plus conditioning with keratin smooth.</p>
+		<p>96 likes</p>
+		<p>2 purchases</p>
+		<a href="">Edit</a>
+		<a href="">Delete</a>
+		<hr />
+	</body>
+</html>
