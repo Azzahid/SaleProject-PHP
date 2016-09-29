9d07533de6b88aa83ca759f32a8f6983354807d5
han_majid <han.majid@yahoo.co.id>
2016-09-29 17:49:32 +0700

    create header template

--- confirmation_purchase.php
+++ confirmation_purchase.php
@@ -74,18 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	</head>
 	<body>
 		<div class="row-center">
-			<h1>SaleProject</h1>
-			<p class="right">Hi, chirastore!<br />
-			<a href="#" class="logout">logout</a></p>
-			<div class="center">
-				<ul>
-					<li><a href=""><span>Catalog</span></a></li>
-					<li><a href=""><span>Your Products</span></a></li>
-					<li><a href=""><span>Add Product</span></a></li>
-					<li><a href=""><span>Sales</span></a></li>
-					<li><a href=""><span>Purchases</span></a></li>
-				</ul>
-			</div>
+			<?php include 'header.php'; ?>
 			<h3>Please confirm your purchase</h3>
 			<hr />
 
---
+++ header.php
@@ -0,0 +1,12 @@
+<h1>SaleProject</h1>
+<p class="right">Hi, chirastore!<br />
+<a href="#" class="logout">logout</a></p>
+<div class="center">
+	<ul>
+		<li><a href=""><span>Catalog</span></a></li>
+		<li><a href=""><span>Your Products</span></a></li>
+		<li><a href=""><span>Add Product</span></a></li>
+		<li><a href=""><span>Sales</span></a></li>
+		<li><a href=""><span>Purchases</span></a></li>
+	</ul>
+</div>
