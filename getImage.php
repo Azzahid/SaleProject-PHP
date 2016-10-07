d99f92df62cbf263851e25ad3c6567d9580683bf
han_majid <han.majid@yahoo.co.id>
2016-10-07 13:36:49 +0700

    edit your products php

--- getImage.php
+++ getImage.php
@@ -2,7 +2,7 @@
 	require_once('db.php');
 	$conn = connect_db();
 	$id_active = $_GET['id_active'];
-	$sql = "SELECT * FROM product WHERE user_id = '$id_active' ";
+	$sql = "SELECT * FROM product WHERE p_id = '$id_active' ";
 	$result = mysqli_query($conn, $sql);
 	$row = mysqli_fetch_array($result);
 	header("Content-type: " . $row["image_type"]);
