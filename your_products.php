b5e01c3c6bfc07f9a217a9b4bc1cf5ad8dafe422
han_majid <han.majid@yahoo.co.id>
2016-10-07 10:44:04 +0700

    edit header.css

--- your_products.php
+++ your_products.php
@@ -1,5 +1,6 @@
 <?php
 	$your_products = true;
+	$id_active = $_GET['id_active'];
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +13,7 @@
 	</head>
 	<body class="body-center helvetica">
 		<?php include 'header.php'; ?>
+		<span id="id_active" <?php echo 'value="'.$id_active.'"' ?>></span>
 		<div class = "border-bottom ">
 			<h2>What are you going to sell today?</h2>
 		</div>
@@ -34,35 +36,11 @@
 				</div>
 				<div class="margin-top">
 					<span class="edit" onclick="edit_item(this.id);" id="1">EDIT</span>
-					<span class="delete" onclick="delete_item()">DELETE</a>
+					<span class="delete" onclick="delete_item(this.id)" id="1">DELETE</a>
 				</div>
 			</div>			
 			<hr class="full" />
-		</div>	
-
-		<div class="product">
-			<strong>Sunday, 14 September 2016</strong><br />
-			at 20.00
-			<hr />
-			<img src="http://placekitten.com/100/100" alt="product-image">
-			<div class="description">
-				<p><strong>Phelps Air Styler</strong><br />
-				<span class="price">IDR 500.000</span><br />
-				Brush shaped hair dryer. Very convenient plus conditioning with keratin smooth.</p>
-			</div>
-
-			<div class="side-description">
-				<p >96 likes<br />
-				2 purchases</p>
-				<span class="edit" onclick="edit_item(this.id);" id="2">EDIT</span>
-				<span class="delete" onclick="delete_item()">DELETE</a>	
-			</div>			
-			<hr class="full" />
 		</div>			
-		<div style="position:relative; border: 1px solid red;width: 40px; height: 40px;">
-   <div style="border:1px solid green;position: absolute; bottom: 0; left: 0; width: 20px; height: 20px;"></div>
-   <div style="border:1px solid blue;position: absolute; bottom: 0; left: 20px; width: 20px height: 20px;"></div>
-</div>
 	</body>
 
 	<script type="text/javascript" src="js/your_products.js"></script>
